863a4264660c9e97bbea33ac3da0697e9d269dcd
Jacek Kawalec <jokerchocolate@gmail.com>
2020-10-26 15:30:50 +0100

    added 5 posts setup

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+import { Link } from 'react-router-dom'
+
 
 
 function Home() {
-    // interface Iposts {
+    // interface Ipost {
     //     [index: number]: object
     // }
     const [posts, setPosts] = useState<any | undefined>([]);
+    const [page, setPage] = useState<any | undefined>([]);
 
     useEffect(() => {
         axios.get("https://jsonplaceholder.typicode.com/posts")
@@ -15,21 +18,24 @@ function Home() {
                 console.log(res)
                 setPosts(res.data)
             })
+            .then(() => setPage(posts.slice(0, 5)))
             .catch(err => {
                 console.log(err)
             })
-    }, [])
+    }, [page])
 
     return (
         <div>
             <ul>
                 {
-                    posts.map((post: {
+                    page.map((post: {
                         id: number | undefined,
                         title: string | undefined
                     }) => <li key={post.id}>{post!.title}</li>)
                 }
             </ul>
+            <li><Link to={`/`}>Back</Link></li>
+            <li><Link to={`/`}>Forward</Link></li>
         </div>
     );
 }
